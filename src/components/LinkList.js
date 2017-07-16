f82e6931578a3d4afcf7ee4f50839f31287496e4
Swarna <swarnakishoree@gmail.com>
2017-07-16 15:05:10 -0500

    implement routes using react router DOM

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,29 +1,44 @@
 import React, { Component } from 'react'
 import Link from './Link'
+import { graphql, gql } from 'react-apollo'
 
 class LinkList extends Component {
 
-  render() {
+ render() {
 
-    const linksToRender = [{
-      id: '1',
-      description: 'The Coolest GraphQL Backend 😎',
-      url: 'https://www.graph.cool'
-    }, {
-      id: '2',
-      description: 'The Best GraphQL Client',
-      url: 'http://dev.apollodata.com/'
-    }]
-
-    return (
-      <div>
-        {linksToRender.map(link => (
-          <Link key={link.id} link={link}/>
-        ))}
-      </div>
-    )
+  // 1
+  if (this.props.allLinksQuery && this.props.allLinksQuery.loading) {
+    return <div>Loading</div>
   }
 
+  // 2
+  if (this.props.allLinksQuery && this.props.allLinksQuery.error) {
+    return <div>Error</div>
+  }
+
+  // 3
+  const linksToRender = this.props.allLinksQuery.allLinks
+
+  return (
+    <div>
+      {linksToRender.map(link => (
+        <Link key={link.id} link={link}/>
+      ))}
+    </div>
+  )
 }
 
-export default LinkList
+}
+const ALL_LINKS_QUERY = gql`
+  # 2
+  query AllLinksQuery {
+    allLinks {
+      id
+      createdAt
+      url
+      description
+    }
+  }
+`
+
+export default graphql(ALL_LINKS_QUERY, { name: 'allLinksQuery' }) (LinkList)
