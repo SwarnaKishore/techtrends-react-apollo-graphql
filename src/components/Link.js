0e9e49e8438064bf7084f199c80cac37a7247c6a
Swarna <swarnakishoree@gmail.com>
2017-07-19 07:11:38 -0500

    more mutations and updating the store

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { GC_USER_ID } from '../constants'
 import { timeDifferenceForDate } from '../utils'
+import { gql, graphql } from 'react-apollo'
 
 class Link extends Component {
 
@@ -20,10 +21,46 @@ render() {
   )
 }
   
-  _voteForLink = async () => {
-    // ... you'll implement this in chapter 6  
+ _voteForLink = async () => {
+  const userId = localStorage.getItem(GC_USER_ID)
+  const voterIds = this.props.link.votes.map(vote => vote.user.id)
+  if (voterIds.includes(userId)) {
+    console.log(`User (${userId}) already voted for this link.`)
+    return
   }
 
+  const linkId = this.props.link.id
+  await this.props.createVoteMutation({
+    variables: {
+      userId,
+      linkId
+    },
+  update: (store, { data: { createVote } }) => {
+    this.props.updateStoreAfterVote(store, createVote, linkId)
+  }
+  })
+}
 }
 
-export default Link
+const CREATE_VOTE_MUTATION = gql`
+  mutation CreateVoteMutation($userId: ID!, $linkId: ID!) {
+    createVote(userId: $userId, linkId: $linkId) {
+      id
+      link {
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`
+
+export default graphql(CREATE_VOTE_MUTATION, {
+  name: 'createVoteMutation'
+})(Link)
