d30e1de82c80add5a407ea0907b0bc44534bc65f
Swarna <swarnakishoree@gmail.com>
2017-07-19 07:59:18 -0500

    implemented search functionality

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import CreateLink from './CreateLink'
 import Header from './Header'
 import { Switch, Route } from 'react-router-dom'
 import Login from './Login'
+import Search from './Search'
 
 class App extends Component {
  render() {
@@ -13,6 +14,7 @@ class App extends Component {
       <Header />
       <div className='ph3 pv1 background-gray'>
         <Switch>
+          <Route exact path='/search' component={Search}/>
           <Route exact path='/' component={LinkList}/>
           <Route exact path='/create' component={CreateLink}/>
           <Route exact path='/login' component={Login}/>
