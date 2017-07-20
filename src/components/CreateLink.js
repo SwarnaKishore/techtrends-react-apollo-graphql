bdee46e850b787d824d8df5075e8146f02637b43
Swarna <swarnakishoree@gmail.com>
2017-07-20 18:57:36 -0500

    added pagination

--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { graphql, gql } from 'react-apollo'
-import { GC_USER_ID } from '../constants'
 import { ALL_LINKS_QUERY } from './LinkList'
+import { GC_USER_ID, LINKS_PER_PAGE } from '../constants'
 
 class CreateLink extends Component {
 
@@ -53,16 +53,24 @@ _createLink = async () => {
       url,
       postedById
     },
-  update: (store, { data: { createLink } }) => {
-    const data = store.readQuery({ query: ALL_LINKS_QUERY })
-    data.allLinks.splice(0,0,createLink)
-    store.writeQuery({
-      query: ALL_LINKS_QUERY,
-      data
-    })
-  }
+    update: (store, { data: { createLink } }) => {
+      const first = LINKS_PER_PAGE
+      const skip = 0
+      const orderBy = 'createdAt_DESC'
+      const data = store.readQuery({
+        query: ALL_LINKS_QUERY,
+        variables: { first, skip, orderBy }
+      })
+      data.allLinks.splice(0,0,createLink)
+      data.allLinks.pop()
+      store.writeQuery({
+        query: ALL_LINKS_QUERY,
+        data,
+        variables: { first, skip, orderBy }
+      })
+    }
   })
-  this.props.history.push(`/`)
+  this.props.history.push(`/new/1`)
 }
 
 }
